8af756ba6341525c8caaa9065b37361d9793ede5
Andre Wang <andrewang87@yahoo.com.au>
2025-02-19 15:16:49 +1100

    reorganised for better usage

--- cdsapi1.py
+++ cdsapi1.py
@@ -1,78 +1,27 @@
+from cdsrequest import request # Your request input.
 import cdsapi
 from eccodes import *
-
-import pygrib
-import matplotlib.pyplot as plt
-import matplotlib.colors as colors
-from mpl_toolkits.basemap import Basemap
-from mpl_toolkits.basemap import shiftgrid
-import numpy as np
-
 import csv
-
-import argparse
-
 import cfgrib
 import xarray as xr
-
 from netCDF4 import Dataset as NetCDFFile 
-#import matplotlib.pyplot as plt
-#import numpy as np
-#from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.basemap import Basemap
+import argparse
 
-from request_params import request
 
-target = "gribs/cdsdata2.grib" #TODO phase this out
-
-def download(outputFilePath):
+def download(outputFileName):
     dataset = "reanalysis-era5-land"
-    request = {
-        "variable": [        
-            "10m_u_component_of_wind",
-            "10m_v_component_of_wind",
-            "surface_pressure",
-            "total_precipitation"
-        ],
-        "year": "2025",
-        "month": "02",
-        "day": ["01"],
-        "time": ["00:00"],
-        "data_format": "grib",
-        "download_format": "unarchived",
-        "area": [-23, 141, -39, 154]
-    }
-
     client = cdsapi.Client()
-    client.retrieve(dataset, request, outputFilePath) #.download()
+    client.retrieve(dataset, request, f'gribs/{outputFileName}.grib') #.download()
+    print(f'GRIB file saved to gribs/{outputFileName}.grib')
 
-def read():
-    # ----- Now read -----
-    f = open(target, 'rb')
-    gid = codes_grib_new_from_file(f)
-
-    values = codes_get_values(gid)
-    num_vals = len(values)
-    for i in range(num_vals):
-        print("%d %.10e" % (i + 1, values[i]))
-
-    print('%d values found in %s' % (num_vals, target))
-
-    for key in ('max', 'min', 'average'):
-        print('%s=%.10e' % (key, codes_get(gid, key)))
-
-    # Example of accessing specific elements from data values
-    # Get first, middle and last elements
-    indexes = [0, int(num_vals/2), num_vals-1]
-    elems = codes_get_double_elements(gid, 'values', indexes)
-
-    codes_release(gid)
-    f.close()
-
-def downloadCSV(gribFilePath, outputFilePath):
+def downloadCSV(gribFileName, outputFileName):
     # Open the GRIB file
-    with open(gribFilePath, 'rb') as f:
+    with open(f'gribs/{gribFileName}.grib', 'rb') as f:
         # Create a CSV file
-        with open(outputFilePath, 'w', newline='') as csvfile:
+        with open(f'csvs/{outputFileName}.csv', 'w', newline='') as csvfile:
             csvwriter = csv.writer(csvfile)
 
             # Initialize header
@@ -111,126 +60,74 @@ def downloadCSV(gribFilePath, outputFilePath):
                     row.append(values[i])
                 csvwriter.writerow(row)
 
-            print(f'{num_vals} values written to {outputFilePath}')
+            print(f'{num_vals} values written to csvs/{outputFileName}.csv')
 
-def toNetCDF():
-    ds = cfgrib.open_file(target)
+def toNetCDF(gribFileName, outputFileName):
+    ds = cfgrib.open_file(f'gribs/{gribFileName}.grib')
 
-    ds = xr.open_dataset(target, engine='cfgrib')
-    ds.to_netcdf("ncs/era5.nc")
+    ds = xr.open_dataset(f'gribs/{gribFileName}.grib', engine='cfgrib')
+    ds.to_netcdf(f'ncs/{outputFileName}.nc')
+    print(f'NetCDF file saved to ncs/{outputFileName}.nc')
 
-def plot3():
-    # Plotting
-    
-    plt.figure(figsize=(12,8))
-    
-    grib = target # Set the file name of your input GRIB file
-    grbs = pygrib.open(grib)
-    
-    grb = grbs.select()[0]
-    data = grb.values
-    
-    # need to shift data grid longitudes from (0..360) to (-180..180)
-    lons = np.linspace(float(grb['longitudeOfFirstGridPointInDegrees']), \
-        float(grb['longitudeOfLastGridPointInDegrees']), int(grb['Ni']) )
-    lats = np.linspace(float(grb['latitudeOfFirstGridPointInDegrees']), \
-        float(grb['latitudeOfLastGridPointInDegrees']), int(grb['Nj']) )
-    # data, lons = shiftgrid(180., data, lons, start=False)
-    data, lons = shiftgrid(lons.max(), data, lons, start=False)
-    grid_lon, grid_lat = np.meshgrid(lons, lats) #regularly spaced 2D grid
-    
-    print(grid_lon, grid_lat)
-
-    m = Basemap(projection='cyl', llcrnrlon=lons.min(), \
-        urcrnrlon=lons.max(),llcrnrlat=lats.min(),urcrnrlat=lats.max(), \
-        resolution='c')
-    
-    x, y = m(grid_lon, grid_lat)
-    
-    cs = m.pcolormesh(x,y,data,shading='flat',cmap=plt.cm.gist_stern_r)
-    
-    m.drawcoastlines()
-    m.drawmapboundary()
-    m.drawparallels(np.arange(-90.,120.,30.),labels=[1,0,0,0])
-    m.drawmeridians(np.arange(-180.,180.,60.),labels=[0,0,0,1])
-    
-   # plt.colorbar(cs,orientation='vertical', shrink=0.5)
-    plt.title('CAMS AOD forecast') # Set the name of the variable to plot
-    #plt.savefig(grib+'.png') # Set the output file name
-    plt.savefig('plots/basemap.png') # Set the output file name
-
-def plot2():
-    ds = cfgrib.open_file(target)
-
-    ds = xr.open_dataset(target, engine='cfgrib')
-    precip = ds.variables['tp']
-    precipitation_da = xr.DataArray(precip)
-    print(precipitation_da)
-    
-    fig = plt.figure(figsize=(10,6))
-    precipitation_da.plot(
-        cmap="Blues",
-        cbar_kwargs={"label": "Total Precipitation [mm]"},
-    )
-    plt.savefig("plots/plot.png") # Set the output file name
-
-def plot():
+def plot(netcdfFileName, variableShortName, outputFileName ):
     # Derived from: https://www2.atmos.umd.edu/~cmartin/python/examples/netcdf_example1.html
-    nc = NetCDFFile('ncs/era5.nc')
-
+    nc = NetCDFFile(f'ncs/{netcdfFileName}.nc')
+    print(nc.variables.keys())
     lat = nc.variables['latitude'][:]
     lon = nc.variables['longitude'][:]
     time = nc.variables['time'][:]
-    t2 = nc.variables['tp'][:] # precipitation
-    #u = nc.variables['10u'][:] # 10m u-component of winds
+    variable = nc.variables[variableShortName][:]
 
     #Let's create a map centered over ___
-    map = Basemap(projection='merc',llcrnrlon=141.,llcrnrlat=-39.,urcrnrlon=154.,urcrnrlat=-23.,resolution='i') # projection, lat/lon extents and resolution of polygons to draw
-    # resolutions: c - crude, l - low, i - intermediate, h - high, f - full
+    latLower = min(lat)
+    latUpper = max(lat)
+    lonLower = min(lon)
+    lonUpper = max(lon)
 
+    map = Basemap(projection='merc',llcrnrlon=lonLower,llcrnrlat=latLower,urcrnrlon=lonUpper,urcrnrlat=latUpper,resolution='i') # projection, lat/lon extents and resolution of polygons to draw
+    # Draw lines for background
     map.drawcoastlines()
     #map.drawrivers(color='lightblue')
     map.drawstates()
     map.drawcountries()
-    map.drawlsmask(land_color='Linen', ocean_color='#CCFFFF') # can use HTML names or codes for colors
-
-    parallels = np.arange(-20,-40,5.) # make latitude lines ever 5 degrees from 30N-50N
-    meridians = np.arange(140, 155,5.) # make longitude lines every 5 degrees from 95W to 70W
+    map.drawlsmask(land_color='Linen', ocean_color='#CCFFFF')
+    # Draw parallels and meridians
+    parallels = np.arange(-90, 90,5.)
+    meridians = np.arange(-180, 180,5.) 
     map.drawparallels(parallels,labels=[1,0,0,0],fontsize=10)
     map.drawmeridians(meridians,labels=[0,0,0,1],fontsize=10)
 
     lons,lats= np.meshgrid(lon,lat) #do not need to shift 180.
     x,y = map(lons,lats)
 
-    plt.title('Precipitation (mm)')
-    print(t2)
-    print(f"Dimensions of t2: {t2.shape}")
-    print(t2[:, :])
-    temp = map.contourf(x, y, t2[:, :], cmap='coolwarm')
+    plt.title(f'{variableShortName} (u)') #TODO make this dynamic
+    temp = map.contourf(x, y, variable, cmap='coolwarm')
     cb = map.colorbar(temp,"right", size="5%", pad="2%")
-    plt.title('Precip')
-    cb.set_label('precipitation (mm)')
+    plt.title(f'{variableShortName} (u)')
+    cb.set_label(f'{variableShortName} (u)')
     
-    plt.savefig('plots/plot3.png')
+    plt.savefig(f'plots/{outputFileName}.png', format='png', dpi=300)
+    print(f'Plot saved to plots/{outputFileName}.png')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process GRIB data.')
     parser.add_argument('--download', metavar='OUTPUT_FILE', help='Download data to the specified file')
-    parser.add_argument('--read', action='store_true', help='Read data')
     parser.add_argument('--csv', nargs=2, metavar=('GRIB_FILE', 'CSV_FILE'), help='Convert GRIB file to CSV')
-    parser.add_argument('--netcdf', action='store_true', help='Convert GRIB file to NetCDF')
-    parser.add_argument('--plot', action='store_true', help='Plot data')
+    parser.add_argument('--netcdf', nargs=2, metavar=('GRIB_FILE', 'NETCDF_FILE'), help='Convert GRIB file to NetCDF')
+    parser.add_argument('--plot', nargs=3, metavar=('NETCDF_FILE', 'VAR_SHORT_NAME', 'PNG_FILE'), help='Plot data')
 
     args = parser.parse_args()
 
     if args.download:
         download(args.download)
-    if args.read:
-        read()
     if args.csv:
-        gribFilePath, outputFilePath = args.csv
-        downloadCSV(gribFilePath, outputFilePath)
+        gribFileName, outputFileName = args.csv
+        downloadCSV(gribFileName, outputFileName)
     if args.netcdf:
-        toNetCDF()
+        gribFileName, outputFileName = args.netcdf
+        toNetCDF(gribFileName, outputFileName)
     if args.plot:
-        plot()
+        netcdfFileName, variableShortName, outputFileName = args.plot
+        #print(variableShortName)
+
+        plot(netcdfFileName, variableShortName, outputFileName)
--- cdsrequests/australia.py
+++ cdsrequests/australia.py
@@ -1,15 +1,13 @@
 request = {
     "variable": [
         "10m_u_component_of_wind",
-        "10m_v_component_of_wind",
-        "surface_pressure",
         "total_precipitation"
     ],
     "year": "2025",
-    "month": "02",
-    "day": ["01"],
-    "time": ["00:00"],
+    "month": "01",
+    "day": ["17"],
+    "time": ["18:00"],
     "data_format": "grib",
     "download_format": "unarchived",
-    "area": [-23, 141, -39, 154]
+    "area": [-10, 113, -44, 154]
 }
